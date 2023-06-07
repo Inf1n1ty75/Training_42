f34b9e6d1be53339e7715088ec4b8312d1bf8865
Inf1n1ty75 <135771100+Inf1n1ty75@users.noreply.github.com>
2023-06-07 02:54:36 +0200

    Add files via upload

---
+++ C_00/ex07/ft_putnbr.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void ft_putchar(char c) {
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int nb) {
+    if (nb == -2147483648) {
+        write(1,"-2147483647", 11);
+        return;
+    }
+    if (nb < 0) {
+        ft_putchar('-');
+        ft_putnbr(nb * -1);
+    }
+    if (nb >= 10) {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+        if (nb <= 9) {
+    ft_putchar(nb + '0');
+    }
+}
+
+int main() {
+    ft_putnbr(-2147483648);
+}
