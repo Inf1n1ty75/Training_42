e87c2f15c3b6ebdc1cc593c0687d0493bb050569
Inf1n1Ty_75 <volkandu75@gmail.com>
2023-06-24 12:27:07 +0200

    ft_swap.c

--- C_01/ex02/ft_swap.c
+++ C_01/ex02/ft_swap.c
@@ -11,14 +11,10 @@ void ft_swap(int *a, int *b) {
 int main() {
     int a;
     int b;
-    int *ptra;
-    int *ptrb;
 
-    a = 7;
-    b = 10;
-    ptra = &a;
-    ptrb = &b; 
-    ft_swap(ptra, ptrb);
-    printf("%d\n", *ptra);
-    printf("%d", *ptrb);
+    a = 10;
+    b = 5;
+    ft_swap(&a, &b);
+    printf("%d\n", a);
+    printf("%d", b);
 }
