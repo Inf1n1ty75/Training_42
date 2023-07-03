56cbd0451d5e13da0134c79f417670a7747f43d0
Inf1n1Ty_75 <volkandu75@gmail.com>
2023-07-03 03:38:22 +0200

    shorter_table_line

--- C_02/ex11/ft_putstr_non_printable.c
+++ C_02/ex11/ft_putstr_non_printable.c
@@ -12,7 +12,8 @@ void ft_putstr_non_printable(char *str) {
 
     deci = 0;
     remain = 0;
-    char base [16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    char base [16] = {'0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     i = 0;
     while (str[i]) {
         if (str[i] >= 0 && str[i] <= 31 || str[i] == 127) {
