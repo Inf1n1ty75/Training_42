d30599dcdf3acb078e206e92122192217347c3d8
Inf1n1Ty_75 <volkandu75@gmail.com>
2023-07-14 00:39:49 +0200

    first

---
+++ C_04/ex04/ft_putnbr_base.c
@@ -0,0 +1,76 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_strlen(char *str) {
+    int i;
+
+    i = 0;
+    while (str[i])
+    i++;
+    return i;
+
+}
+
+void ft_putchar(char c) {
+    write(1, &c, 1);
+}
+
+int ft_check_base(int nbr, char *base) {
+    int i;
+    int j;
+    int base_len;
+
+    i = 0;
+    j = 1;
+    base_len = ft_strlen(base);
+
+    if (base_len <= 1 || base[i] == '\0')
+    return 1;
+    while (base[i]) {
+        if (base[i] == '+' || base[i] == '-')
+        return 1;
+        i++;
+    }
+    i = 0;
+    while (base[i]) {
+        if (base[i] == base[j + i])
+        return 1;
+        i++;
+    }
+    return 0;
+}
+
+void ft_putnbr_base(int nbr, char *base) {
+    int len_base;
+    int i;
+    char result [100];
+
+    len_base = ft_strlen(base);
+    i = 0;
+    if (ft_check_base(nbr, base) == 1) {
+        nbr = 0;
+        ft_putchar('0');
+    }
+    if (nbr < 0) {
+        nbr *= -1;
+        ft_putchar('-');
+    }
+    while (nbr > 0) {
+        result[i] = base[nbr % len_base];
+        nbr = nbr / len_base;
+        i++;
+    }
+    while (i > 0) {
+        ft_putchar(result[i - 1]);
+        i--;
+    }
+}
+
+int main() {
+    int nbr;
+    char base [] = "0123456789ABCDEF";
+
+    nbr = 42;
+    ft_putnbr_base(nbr, base);
+}
